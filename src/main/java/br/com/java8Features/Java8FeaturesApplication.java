891a3452b8c813d274a1983d92fcb797123a19df
tliner-friaca-dev <tlinerfc@hotmail.com>
2023-03-10 08:42:37 -0300

    first commit

---
+++ src/main/java/br/com/java8Features/Java8FeaturesApplication.java
@@ -0,0 +1,18 @@
+package br.com.java8Features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8FeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8FeaturesApplication.class, args);
+		
+		System.out.println("\n----------------------");
+		System.out.println("ENTROU NA APLICAÇÃO!!!");
+		System.out.println("----------------------\n");
+
+	}
+
+}
